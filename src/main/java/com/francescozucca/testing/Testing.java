881059ad4a297f121cad72fbd8832f607bbf3ea6
Francesco Zucca <francescozucca30@gmail.com>
2020-02-29 14:27:57 +0100

    added resources

---
+++ src/main/java/com/francescozucca/testing/Testing.java
@@ -0,0 +1,13 @@
+package com.francescozucca.testing;
+
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * Created by Francesco on 29/02/2020.
+ * @author Francesco
+ * The main class of the mod.
+ */
+@Mod(Testing.MOD_ID)
+public class Testing {
+    public static final String MOD_ID = "testing";
+}
